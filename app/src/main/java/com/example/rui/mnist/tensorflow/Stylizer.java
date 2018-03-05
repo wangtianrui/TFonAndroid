26a8b777d9b42e45d0a14198497f36c4297f2d86
ScorpioMiku <1056992492@qq.com>
2018-03-05 22:06:35 +0800

    1

--- app/src/main/java/com/example/rui/mnist/tensorflow/Stylizer.java
+++ app/src/main/java/com/example/rui/mnist/tensorflow/Stylizer.java
@@ -1,6 +1,9 @@
 package com.example.rui.mnist.tensorflow;
 
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
 
 import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
@@ -9,7 +12,9 @@ import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
  */
 
 public class Stylizer {
-    private int[] intValues;
+
+    private String TAG = "Stylizer";
+    private int[] intValues = new int[256*256];
     private float[] floatValues;
     private TensorFlowInferenceInterface inferenceInterface;
 
@@ -18,9 +23,17 @@ public class Stylizer {
     private static final String STYLE_NAME = "style_num";
     private static final String OUTPUT_NAME = "transformer/expand/conv3/conv/Sigmoid";
     private static final int NUM_STYLES = 26;
+    private float[] styleVals = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f
+            , 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
 
+    public Stylizer(AssetManager assetManager) {
+        inferenceInterface = new TensorFlowInferenceInterface(assetManager, MODEL_FILE);
+    }
 
-    private void stylizeImage(final Bitmap bitmap) {
+    public Bitmap stylizeImage(String path, int styleValue) {
+        Bitmap bitmap = BitmapFactory.decodeFile(path);
+        styleVals[styleValue] = 1.0f;
+        Log.d(TAG, "stylizeImage: "+bitmap.getWidth());
         bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
         for (int i = 0; i < intValues.length; ++i) {
             final int val = intValues[i];
@@ -28,14 +41,15 @@ public class Stylizer {
             floatValues[i * 3 + 1] = ((val >> 8) & 0xFF) / 255.0f;
             floatValues[i * 3 + 2] = (val & 0xFF) / 255.0f;
         }
-        // Copy the input data into TensorFlow.
+        Log.d(TAG, "stylizeImage: f"+floatValues[1]);
+
         inferenceInterface.feed(
                 INPUT_NAME, floatValues, 1, bitmap.getWidth(), bitmap.getHeight(), 3);
-        //inferenceInterface.feed(STYLE_NAME, styleVals, NUM_STYLES);
+        inferenceInterface.feed(STYLE_NAME, styleVals, NUM_STYLES);
 
-        //inferenceInterface.run(new String[]{OUTPUT_NAME}, isDebug());
+        inferenceInterface.run(new String[]{OUTPUT_NAME}, false);
         inferenceInterface.fetch(OUTPUT_NAME, floatValues);
-
+        Log.d(TAG, "stylizeImage: "+STYLE_NAME);
         for (int i = 0; i < intValues.length; ++i) {
             intValues[i] =
                     0xFF000000
@@ -43,7 +57,8 @@ public class Stylizer {
                             | (((int) (floatValues[i * 3 + 1] * 255)) << 8)
                             | ((int) (floatValues[i * 3 + 2] * 255));
         }
-
+        Log.d(TAG, "stylizeImage:i "+intValues[1]);
         bitmap.setPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        return bitmap;
     }
 }
